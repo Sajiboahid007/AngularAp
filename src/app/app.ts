21f06ff0e94b56cecede63f6646694d663fd69fe
Sajiboahid007 <sajiboahid007@gmail.com>
2025-11-12 20:10:04 +0600

    tutorial15

--- src/app/app.ts
+++ src/app/app.ts
@@ -10,31 +10,13 @@ import { Singup } from './singup/singup';
   styleUrl: './app.css',
 })
 export class App {
-  count = 0;
-  decrement() {
-    this.count--;
-    if (this.count < 0) {
-      this.count = 0;
-    }
-    alert('counter can be negative');
-  }
-  increment() {
-    this.count++;
-  }
-  reset() {
-    this.count = 0;
-    alert('are you sure!');
-  }
+    color = 1;
 
-  public handleCounter(value: number) {
-    if (value === 0) {
-      this.count = 0;
-    } else {
-      this.count += value;
-      if (this.count < 0) {
-        this.count = 0;
-        alert('counter can be negative');
-      }
+    public handleColor(value:number){
+      this.color = value
     }
-  }
+
+    public handleInput(event: Event){
+      this.color = parseInt((event.target as HTMLInputElement).value)
+    }
 }
